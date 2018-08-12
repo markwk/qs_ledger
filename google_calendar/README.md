da24237c5036972961e170eddd71352703a78165
Mark Koester <mark@int3c.com>
2018-08-12 19:00:42 +0800

    google_calendar_downloader initial commit

---
+++ google_calendar/README.md
@@ -0,0 +1,28 @@
+# Google Calendar
+
+* Download and Export your past calendar events to CSV
+* Coming Soon: Data analysis and data visualization of your calendar activities!
+
+## Initial Install and Setup
+
+Follow Steps here: https://developers.google.com/calendar/quickstart/python
+
+1. Click Button "ENABLE THE GOOGLE CALENDAR API"
+2. Select + Create a new project.
+3. Download the configuration file.
+4. Move the downloaded file to your working directory and ensure it is named credentials.json.
+5. In terminal or command line, navigate to your project's working directory
+6. Run `python quickstart.py`
+
+Afterwards:
+
+> The sample will attempt to open a new window or tab in your default browser. If this fails, copy the URL from the console and manually open it in your browser.
+
+> If you are not already logged into your Google account, you will be prompted to log in. If you are logged into multiple Google accounts, you will be asked to select one account to use for the authorization.
+
+> Click the Accept button.
+> The sample will proceed automatically, and you may close the window/tab.
+
+## Usage
+
+* Download your data via [Google Calendar Downloader](https://github.com/markwk/qs_ledger/blob/master/google_calendar/google_calendar_downloader.ipynb)
---
+++ google_calendar/quickstart.py
@@ -0,0 +1,36 @@
+from __future__ import print_function
+import datetime
+from googleapiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
+
+def main():
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    store = file.Storage('token.json')
+    creds = store.get()
+    if not creds or creds.invalid:
+        flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+        creds = tools.run_flow(flow, store)
+    service = build('calendar', 'v3', http=creds.authorize(Http()))
+
+    # Call the Calendar API
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    print('Getting the upcoming 10 events')
+    events_result = service.events().list(calendarId='primary', timeMin=now,
+                                        maxResults=10, singleEvents=True,
+                                        orderBy='startTime').execute()
+    events = events_result.get('items', [])
+
+    if not events:
+        print('No upcoming events found.')
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        print(start, event['summary'])
+
+if __name__ == '__main__':
+    main()
